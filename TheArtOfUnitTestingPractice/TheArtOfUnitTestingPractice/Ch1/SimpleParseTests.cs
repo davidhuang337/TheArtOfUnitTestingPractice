fa1ffe2de754acfc123ba9b00b8cebe293b02ab6
david.huang <guohuang107@gmail.com>
2019-02-17 20:10:43 +0800

    add SimpleParser and tests

---
+++ TheArtOfUnitTestingPractice/TheArtOfUnitTestingPractice/Ch1/SimpleParseTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheArtOfUnitTestingPractice.Ch1
+{
+    class SimpleParseTests
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            try
+            {
+                TestReturnZeroWhenEmptyString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);                
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void TestReturnZeroWhenEmptyString()
+        {
+            try
+            {
+                var simpleParser = new SimpleParser();
+                var result = simpleParser.ParseAndSum(string.Empty);
+                if (result != 0)
+                {
+                    Console.WriteLine(
+                        @"***SimpleParsweTests.TestReturnZeroWhenEmptyString:
+                         -----
+                        Parse and sum should have returned 0 on an empty ");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ TheArtOfUnitTestingPractice/TheArtOfUnitTestingPractice/Ch1/SimpleParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheArtOfUnitTestingPractice.Ch1
+{
+    class SimpleParser
+    {
+        public int ParseAndSum(string numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            if (!numbers.Contains(","))
+            {
+                return int.Parse(numbers);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
